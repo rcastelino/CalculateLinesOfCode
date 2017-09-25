704755d628d353a2720a61ef78dd18060116020b
Roland Castelino <rolypoly123@gmail.com>
2017-09-25 10:41:05 -0400

    Added python script plus executable to check lines of code, updated README

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# CalculateLinesOfCode
-Python : script to 'Actual' calculate lines of code for all major languages (java, ruby, objective-c, python, javascript)
+# Introduction - Calculate 'Actual' Lines Of Code
+
+Python script: useful utility to calculate lines of code for all major languages (java, ruby, objective-c, python, javascript)
+
+Anyone who writes code wants to have less lines to meet the objective of the program. This utility script helps to identifying real lines of code and prints out type of file and language, comments (single and multi-line), empty / blank lines, actual lines of code and total LOC; for each file in the directory.
+
+# Usage:
+- on MacOS / Linux, simply copy the executable in the code root direcotry and execute it (double click). 
+- on Windows, copy the .py file in the code root directory and execute it via command prompt. (Pre-req - python.exe to be installed. More details --> https://docs.python.org/3/faq/windows.html)
+
+# Supports Python version 2 and 3
+
+## Sample Console Output
+(This output is when executed from a directory having .py and .java file)
+
+******************************************
+
+File --->  countLOC_python_v3.py  --->  python
+
+Single line comment   : 8
+
+Multi line block      : 1  ( 14 ) comment lines in block
+
+Empty or Blank lines  : 13
+
+Actual lines of Code  : 96
+
+Total lines in file   : 131
+
+./countLOC_python_v2.py : 96
+
+------------------------------------------
+
+File --->  sampleTestFile.java  --->  java
+
+Single line comment   : 21
+
+Multi line block      : 2  ( 19 ) comment lines in block
+
+Empty or Blank lines  : 28
+
+Actual lines of Code  : 110
+
+Total lines in file   : 178
+
+./sampleTestFile.java : 110
+
+------------------------------------------
+
+
+Number of Files : 2
+
+Total Actual Lines of Code : 206
+
+******************************************
+
+# MIT License
+
+Copyright (c) 2017 Roland Castleino
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ countLOC_python_v2.py
@@ -0,0 +1,131 @@
+#! /usr/bin/python
+'''
+THIS CODE IS ONLY FOR PYTHON VERSION 2
+***************************************************************************************
+Objective: Utility Script - To count Actual Lines of Code in a file.
+Notes: Counts LOC, single/multi line comments, blank lnes, for 'javascript, python, ruby, java'
+Usage: python countLOC_python_v2.py
+---------------------------------------------------------------------------------------
+History:     Date:               Author:                 Update / Comments
+---------------------------------------------------------------------------------------
+1)       27th Jul 2014      Roland Castelino           Initial creation. supports javascript file
+2)       19th Oct 2014      Roland Castelino           Added support for ruby, python, java (dictonary created)
+                                                       Minor code changes
+***************************************************************************************
+'''
+
+import os, sys
+
+# Files accepted: javascript, ruby, python, java
+fileType = ['.js', '.rb', '.py', '.java']
+myPath = os.path.abspath(os.path.dirname(sys.argv[0]))
+lang_dictonary = {
+    '.js':{
+            'language'      : "javascript",
+            'multi_start'   : "/*",
+            'multi_end'     : "*/",
+            'single_line'   : "//",    
+        },
+    '.rb':{
+            'language'      : "ruby",
+            'multi_start'   : "=begin",
+            'multi_end'     : "=end",
+            'single_line'   : "#",                
+        },
+    '.py':{
+            'language'      : "python",
+            'multi_start'   : "'''",
+            'multi_end'     : "'''",
+            'single_line'   : "#",    
+        },
+    '.java':{
+            'language'      : "java",
+            'multi_start'   : "/*",
+            'multi_end'     : "*/",
+            'single_line'   : "//",     
+        }
+    }
+
+
+def get_code_language():
+    key = ''
+    for code in lang_dictonary.keys():
+        if ext == code:
+            key = code
+            break
+    return lang_dictonary[key]
+
+
+def read_line_count(fname):
+    code_type = get_code_language()
+
+    # Counters to calculate Lines
+    countBlank = 0
+    countComments = 0
+    countCommentsBlock = 0
+    countTotalLOC = 0
+    countTotalLines = 0
+    multiCount = 0
+    
+    # Lines stored in an array
+    myLines = open(fname).readlines()
+    myNewLines = []
+
+    # to make True when Comment Block encountered
+    boolCommentBlock = False;
+    countTotalLines = len(myLines)
+    # Calculate number of Comments, Comment Blocks, Blank Lines, LOC
+    for line in myLines:
+        line = line.strip()
+        if not boolCommentBlock and line.startswith(code_type['multi_start']):
+            boolCommentBlock = True
+            countCommentsBlock += 1
+        elif(not boolCommentBlock):
+            if line.startswith(code_type['single_line']):
+                countComments += 1
+            elif len(line.strip())>0:
+                countTotalLOC += 1
+            else:
+                countBlank += 1
+        # make boolCommentBlock False when '*/' encountered
+        elif boolCommentBlock and line.startswith(code_type['multi_end']) or line.find(code_type['multi_end'])>=0:
+            boolCommentBlock = False
+    
+    multiCount = countTotalLines - countComments - countTotalLOC - countBlank
+    print "File ---> ",path[len(myPath):].split("/")[-1]," ---> ",code_type['language']
+    print "Single line comment   :", countComments
+    if multiCount == 0:
+        print "Multi line block      :", countCommentsBlock
+    else:
+        print "Multi line block      :", countCommentsBlock, " (", multiCount,") comment lines in block"
+    print "Empty or Blank lines  :", countBlank
+    print "Actual lines of Code  :", countTotalLOC
+    print "Total lines in file   :", countTotalLines
+
+    return countTotalLOC
+
+
+if __name__ == '__main__':
+    print "******************************************"
+    total_loc_count = 0
+    file_count = 0
+    for base, dirs, files in os.walk(myPath):
+        for file in files:
+            # Check the sub directorys
+            if file.find('.') < 0:
+                continue
+            ext = (file[file.rindex('.'):]).lower()
+            try:
+                if fileType.index(ext) >= 0:
+                    file_count += 1
+                    path = (base + '/'+ file)
+                    c = read_line_count(path)
+                    print "------------------------------------------"
+                    print ".%s : %d" % (path[len(myPath):], c)
+                    print "------------------------------------------\n"
+                    total_loc_count += c
+            except:
+                pass
+print 'Number of Files : %d' % file_count
+print 'Total Actual Lines of Code : %d' % total_loc_count
+print "******************************************"
